6eee2869bafaca2f45e8a007fc641b941e775df7
Parth Parmar <m2parthparmar@gmail.com>
2021-11-24 14:12:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/dishes", dishRouter);
+// app.use("/dishes", dishRouter);
 app.use("/leaders", leaderRouter);
 app.use("/promotions", promoRouter);
 
